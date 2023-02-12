3bca52bab0e67d06ebd5b8285facd2aee06c5e4f
Eunse Jang <senuej37@gmail.com>
2023-02-12 22:29:14 +0900

    23.02.12

---
+++ Silver/10773.java
@@ -0,0 +1,35 @@
+package Silver;
+
+import java.util.*;
+
+class B10773 {
+  public static void main(String[] args){
+    Scanner scanner=new Scanner(System.in);
+
+    Stack<Integer> stack=new Stack<>();
+    int test=scanner.nextInt();
+
+    for(int i=0;i<test;i++){
+      int num=scanner.nextInt();
+      if(num==0){
+        stack.pop();
+      }
+      else{
+        stack.push(num);
+      }
+    }
+
+    int sum=0;
+
+    for(int i=0;i<stack.capacity();i++){
+      if(stack.isEmpty()){
+        break;
+      }
+      else{
+        sum+=stack.peek();
+      }
+      stack.pop();
+    }
+    System.out.println(sum);
+  }
+}
