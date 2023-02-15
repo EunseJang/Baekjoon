4fc60cf2da7c8f8234b824e4ca1c7866fed1a900
Eunse Jang <senuej37@gmail.com>
2023-02-15 14:51:37 +0900

    23.02.15

---
+++ Silver/2164.java
@@ -0,0 +1,21 @@
+package Silver;
+
+import java.util.*;
+
+class B2164 {
+  public static void main(String[] args){
+    Scanner scanner = new Scanner(System.in);
+
+    int N=scanner.nextInt();
+    Deque<Integer> deque=new LinkedList<>();
+
+    for(int i=1; i<=N;i++){
+      deque.addLast(i);
+    }
+    for (int i=0;i<N-1;i++){
+      deque.removeFirst();
+      deque.addLast(deque.pollFirst());
+    }
+    System.out.println(deque.peekFirst());
+  }
+}
